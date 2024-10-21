68aaa6228960dd13a30eb90461d6d5924e42a21e
Samukelo Gift Msimanga <sam@Samukelos-MacBook-Air.local>
2024-10-21 04:54:12 +0200

    Just added the Test 1 A From this other guy...

---
+++ Test 1 A/Q6.py
@@ -0,0 +1,11 @@
+# Discrete is dating Numpy ***
+import numpy as np
+
+## Replicate the graph!
+
+# Create the n - axit values
+
+n = np.arange(-8, 8)
+x = np.array([0, 0, 0, 1, 2, 3, 1, 1, 1, 0, -1, -2, -2, 0, 0, 0])
+print(n)
+print(x)
