294bd18f50973718c637c03daf599eeba6f951dc
82Sam <Ivykiid@gmail.com>
2024-11-10 16:44:05 +0200

    Closing this library

---
+++ Test 1/Main.java
@@ -0,0 +1,20 @@
+public class Dog {
+    // instance variables
+    String breed;
+    String size;
+    String color;
+    int age;
+
+    // methods
+    public void eat() {
+        System.out.println("The dog is eating.");
+    }
+
+    public void run() {
+        System.out.println("The dog is running.");
+    }
+
+    public void sleep() {
+        System.out.println("The dog is sleeping.");
+    }
+}
